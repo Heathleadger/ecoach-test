2562d9411c34c29683ac379c153412925bfb001b
Vinicius Souza <viniciussouzaat@gmail.com>
2018-03-03 21:44:16 +0000

    association of user - articles implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+    has_many :articles
+    before_save { self.email = email.downcase }
+    validates :username, presence: true, uniqueness: { case_sensitive: false}, length: {minimum: 6, maximum:25}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: { case_sensitive: false}, length: {minimum: 6, maximum:50}, format: { with: VALID_EMAIL_REGEX }
     
 end
