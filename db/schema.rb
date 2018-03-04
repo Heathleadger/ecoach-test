1e6d98417efbfbfd538169e7afe3d02f60364d5f
Vinicius Souza <viniciussouzaat@gmail.com>
2018-03-04 17:20:45 +0000

    Add users password backend

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303211735) do
+ActiveRecord::Schema.define(version: 20180304171301) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180303211735) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
