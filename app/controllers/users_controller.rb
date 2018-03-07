e15923788660f3ffaf3d7f60dd037f559ffe169e
Vinicius Souza <viniciussouzaat@gmail.com>
2018-03-07 15:41:44 +0000

    add authentication in UI and controller level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,61 @@
 class UsersController < ApplicationController
-    def index
-     @users = User.paginate(page: params[:page], per_page: 5)
+  
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+  
+  def index
+   @users = User.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        flash[:success] = "Welcome to E-Coach Mr.(s) #{@user.username}"
+        redirect_to articles_path
+    else
+      render 'new'
     end
-    
-    def new
-      @user = User.new
-    end
-    
-    def create
-      @user = User.new(user_params)
-      if @user.save
-          flash[:success] = "Welcome to E-Coach Mr.(s) #{@user.username}"
-          redirect_to articles_path
-      else
-        render 'new'
-      end
-    end
-    
-    def edit
-       @user = User.find(params[:id]) 
-    end
-    
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-         flash[:success] = "You profile was updated"
-         redirect_to articles_path
-      else
-          render 'edit'
-      end
-    end
+  end
+  
+  def edit
 
-    def show
-      @user = User.find(params[:id])
-      @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def update
+    if @user.update(user_params)
+       flash[:success] = "You profile was updated"
+       redirect_to articles_path
+    else
+        render 'edit'
     end
+  end
 
-    private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
+  def show
+    if !logged_in? || current_user != @user
+      flash[:danger] = "You are not logged in with this user"
+      redirect_to articles_path
     end
-    
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+   if !logged_in? || current_user != @user
+     flash[:danger] = "You are not the user of this account"
+     redirect_to articles_path
+   end
+  end
 end
