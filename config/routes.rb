d23505bc4d84445965069ea5eab8c53d3cf228dd
Vinicius Souza <viniciussouzaat@gmail.com>
2018-03-11 16:52:30 +0000

    added categories controler, model, view for index and new

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
+  resources :categories
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
